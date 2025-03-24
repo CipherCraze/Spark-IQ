a4e4e2715b631603c1cc53f70071dc4c8fc58cab
Noel Manoj <noelmanoj24bcs46@iiitkottayam.ac.in>
2025-03-24 09:09:02 +0530

    add GradingSystem component and route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ import PersonalizedFeedback from './Components/Features/Personalized_feedback';
 import NotFound from './Components/NotFound';
 import EducatorDashboard from './Components/dashboard/EducatorDashboard';
 import AssignmentManagement from './Components/Features/AssignmentManagement';
+import GradingSystem from './components/Features/GradingSytem';
 
 const Layout = ({ children, showHeaderFooter = true }) => (
   <div className="flex flex-col min-h-screen">
@@ -89,6 +90,15 @@ function App() {
           element={
             <Layout showHeaderFooter={false}> {/* Hide Header and Footer */}
               <AssignmentManagement />
+            </Layout>
+          }
+        />
+        {/* Grading System */}
+        <Route
+          path="/grading-system"
+          element={
+            <Layout showHeaderFooter={false}> {/* Hide Header and Footer */}
+              <GradingSystem />
             </Layout>
           }
         />
